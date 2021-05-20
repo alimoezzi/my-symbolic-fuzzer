348e5c84427a015b1a2ddabd63fb6633e765d14e
Mirana <Mini_Mirana@yahoo.com>
2021-05-20 20:05:40 +0300

    REMOVE ASSERTION ERROR FOR UNSATISFIED CORES

--- MySymbolicFuzzer.py
+++ MySymbolicFuzzer.py
@@ -5,3 +5,13 @@ from utils import to_single_assignment_predicates, identifiers_with_types, to_sr
 
 class MySymbolicFuzzer(AdvancedSymbolicFuzzer):
 
+    # REMOVE ASSERTION ERROR FOR UNSATISFIED CORES
+    def extract_constraints(self, path):
+        res = []
+        generated_paths, completed = to_single_assignment_predicates(path)
+        if not completed:
+            return []
+        for p in generated_paths:
+            if p:
+                res.append(to_src(p))
+        return res
