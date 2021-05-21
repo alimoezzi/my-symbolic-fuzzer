fe84787480e5c6dd8bcf78e7786c7ef24d4cbee2
ALi <s.arme@ya.ru>
2021-05-21 15:50:37 +0300

    Implement a_check_triangle_book_example example code

---
+++ examples/a_check_triangle_book_example.py
@@ -0,0 +1,17 @@
+def a_check_triangle_book_example(a: int, b: int, c: int) -> int:
+    if a == b:
+        if a == c:
+            if b == c:
+                return "Equilateral"
+            else:
+                return "Isosceles"
+        else:
+            return "Isosceles"
+    else:
+        if b != c:
+            if a == c:
+                return "Isosceles"
+            else:
+                return "Scalene"
+        else:
+            return "Isosceles"
