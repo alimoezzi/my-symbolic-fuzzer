2cd64735c484282856b8c1937e118cc89885dc34
vahid <vah.torabi@gmail.com>
2021-05-30 14:35:43 +0300

    get input values from user

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ def myInput(input_name, type):
             print(f"{str(user_input)} must be a/an {str(type)}")
 
 if __name__ == "__main__":
+    max_depth = myInput("max_depth", int)
+    max_tries = myInput("max_tries", int)
+    max_iter = myInput("max_iter", int)
+    example_program = myInput("example_program", str)
+    function_name = myInput(f"function from {example_program} program", str)
